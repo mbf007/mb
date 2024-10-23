6803756953df4b3e6ef2b8225f5a283e1f5d380a
Andrea Giacobino <no.andrea@gmail.com>
2024-10-23 13:50:44 +0200

    add minimal binary creation

---
+++ minimal-binary/src/main.rs
@@ -0,0 +1,13 @@
+use log::{debug, error, info, trace, warn};
+
+fn main() {
+    // Initialize the logger
+    env_logger::init();
+    println!("Hello!");
+
+    // Log messages with different severity levels
+    info!("This is an INFO level message.");
+
+    // Print a standard output message
+    println!("Goodbye");
+}
